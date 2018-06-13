665750f980b568fc4058b98488d71c7899f212b9
Cadman <jcadiente505@gmail.com>
2018-06-13 14:16:27 -0400

    started working on EU section/background image bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -49,55 +49,55 @@ var questDisney = [
 		photo: "assets/images/rogue-squadron.jpg"
 	}];
 
-// var questEU = [
-// 	{
-// 		question: "Which Jedi Knight was the Hero of the Mandalorian Wars?", 
-// 		choice: ["Malak", "Revan", "Meetra Surik", "Bastila Shan"],
-// 		answer: 1,
-// 		photo: "assets/images/revan-image.png"
-// 	 },
-// 	 {
-// 		 question: "Darth Caedus real name is what?", 
-// 		choice: ["Jacen Solo", "Ben Solo", "Jaina Solo", "Ben Skywalker"],
-// 		answer: 0,
-// 		photo: "assets/images/darth-caedus-image.jpg"
-// 	 }, 
-// 	 {
-// 		 question: "The wife of Luke Skywalker is?", 
-// 		choice: ["Leia Organa", "Mara Jade", "Rey", "Satele Shan" ],
-// 		answer: 1,
-// 		photo: "assets/images/mara-jade-image.jpg"
-// 	}, 
-// 	{
-// 		question: "What race of Aliens nearly destroyed the New Republic and despised technology?", 
-// 		choice: ["Chiss", "Rancor", "Yuuzhan Vong", "Twi'lek" ],
-// 		answer: 2,
-// 		photo: "assets/images/yuuzhan-vong-image.jpg"
-// 	}, 
-// 	{
-// 		question: "Darth Nihilus was part of what Sith group?", 
-// 		choice: ["Empire", "Rebellion", "Horde", "Triumvirate" ],
-// 		answer: 3,
-// 		photo: "assets/images/darth-nihilus-image.jpg"
-// 	}, 
-// 	{
-// 		question: "in the Epic Battle of Uroro Station, 41ABY, which Jedi killed Darth Caedus?", 
-// 		choice: ["Luke Skywalker", "Jaina Solo", "Ben Skywalker", "Han Solo" ],
-// 		answer: 1,
-// 		photo: "assets/images/jaina-jacen-image.jpg"
-// 	}, 
-// 	{
-// 		question: "Which Chiss Admiral was considered a tactical genius and rallied the Empire Remnants?", 
-// 		choice: ["Grand Moff Tarkin", "Grand Admiral Thrawn", "Darth Vader", "Supreme Leader Snoke" ],
-// 		answer: 1,
-// 		photo: "assets/images/thrawn-image.jpg"
-// 	}, 
-// 	{
-// 		question: "Which Jedi Master initially started as a stormtrooper before being trained by Luke?", 
-// 		choice: ["Kyle Katarn", "Han Solo", "Finn", "Captain Phasma" ],
-// 		answer: 0,
-// 		photo: "assets/images/kyle-katarn-image.jpg"
-// 	}];
+var questEU = [
+	{
+		question: "Which Jedi Knight was the Hero of the Mandalorian Wars?", 
+		choice: ["Malak", "Revan", "Meetra Surik", "Bastila Shan"],
+		answer: 1,
+		photo: "assets/images/revan-image.png"
+	 },
+	 {
+		 question: "Darth Caedus real name is what?", 
+		choice: ["Jacen Solo", "Ben Solo", "Jaina Solo", "Ben Skywalker"],
+		answer: 0,
+		photo: "assets/images/darth-caedus-image.jpg"
+	 }, 
+	 {
+		 question: "The wife of Luke Skywalker is?", 
+		choice: ["Leia Organa", "Mara Jade", "Rey", "Satele Shan" ],
+		answer: 1,
+		photo: "assets/images/mara-jade-image.jpg"
+	}, 
+	{
+		question: "What race of Aliens nearly destroyed the New Republic and despised technology?", 
+		choice: ["Chiss", "Rancor", "Yuuzhan Vong", "Twi'lek" ],
+		answer: 2,
+		photo: "assets/images/yuuzhan-vong-image.jpg"
+	}, 
+	{
+		question: "Darth Nihilus was part of what Sith group?", 
+		choice: ["Empire", "Rebellion", "Horde", "Triumvirate" ],
+		answer: 3,
+		photo: "assets/images/darth-nihilus-image.jpg"
+	}, 
+	{
+		question: "in the Epic Battle of Uroro Station, 41ABY, which Jedi killed Darth Caedus?", 
+		choice: ["Luke Skywalker", "Jaina Solo", "Ben Skywalker", "Han Solo" ],
+		answer: 1,
+		photo: "assets/images/jaina-jacen-image.jpg"
+	}, 
+	{
+		question: "Which Chiss Admiral was considered a tactical genius and rallied the Empire Remnants?", 
+		choice: ["Grand Moff Tarkin", "Grand Admiral Thrawn", "Darth Vader", "Supreme Leader Snoke" ],
+		answer: 1,
+		photo: "assets/images/thrawn-image.jpg"
+	}, 
+	{
+		question: "Which Jedi Master initially started as a stormtrooper before being trained by Luke?", 
+		choice: ["Kyle Katarn", "Han Solo", "Finn", "Captain Phasma" ],
+		answer: 0,
+		photo: "assets/images/kyle-katarn-image.jpg"
+	}];
 // variables too use
 var correct = 0;
 var wrong = 0;
@@ -107,13 +107,13 @@ var intervalId;
 var userGuess = "";
 var running = false;
 var questCounterDisney = questDisney.length;
-// var questCounterEU = questEU.length;
+var questCounterEU = questEU.length;
 var pick;
 var index;
 var newArray = [];
 var holder = [];
 
-
+$("#reset").hide()
 //Disney Section!
 $("#disney").on("click", function () {
 	$("#disney").hide();
@@ -227,14 +227,14 @@ $(".answerchoice").on("click", function () {
 		stop();
 		correct++;
 		userGuess = "";
-		$("#answersection").html("<p>Correct!</p>");
+		$("#answersection").html("<h3>Correct!</h3>");
 		hidebackground();
 
 	} else {
 		stop();
 		wrong++;
 		userGuess = "";
-		$("#answersection").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+		$("#answersection").html("<h3>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</h3>");
 		hidebackground();
 	}
 })
@@ -254,57 +254,74 @@ $("#reset").on("click", function () {
 
 
 
-	// $("#EU").on("click", function () {
-	// 	$("#disney").hide();
-	// 	$("#EU").hide();
-	// 	displayQuestionEU();
-	// 	timeStart();
-	// 	for(var i = 0; i < questEU.length; i++) {
-	// 		holder.push(questEU[i]);
-	// 		}
-	// })
+	$("#EU").on("click", function () {
+		$("#disney").hide();
+		$("#EU").hide();
+		displayQuestionEU();
+		timeStart();
+		for(var i = 0; i < questEU.length; i++) {
+			holder.push(questEU[i]);
+			}
+	})
 
-	// function displayQuestionEU() {
-	// 	index = Math.floor(Math.random()*questEU.length);
-	// 	pick = questEU[index];
-	// 	$("body").css("background-image", "url(" + pick.photo + ")");
+	function displayQuestionEU() {
+		index = Math.floor(Math.random()*questEU.length);
+		pick = questEU[index];
+		$("body").css("background-image", "url(" + pick.photo + ")");
 
-	// 		$("#questionsection").html("<h2>" + pick.question + "</h2>");
-	// 		for(var i = 0; i < pick.choice.length; i++) {
-	// 			var userChoice = $("<button>");
-	// 			userChoice.addClass("answerchoice");
-	// 			userChoice.addClass("btn btn-dark btn-sm")
-	// 			userChoice.html(pick.choice[i]);
-	// 			userChoice.attr("data-guessvalue", i);
-	// 			$("#answersection").append(userChoice);
+			$("#questionsection").html("<h2>" + pick.question + "</h2>");
+			for(var i = 0; i < pick.choice.length; i++) {
+				var userChoice = $("<button>" + pick.choice[i] + "</button>").on("click", function (){ 
 
-	// 			}
-	// 	}
+					userGuess = parseInt($(this).attr("data-guessvalue"));
+	
+					console.log(userGuess)
+					if (userGuess === pick.answer) {
+					stop();
+					correct++;
+					userGuess = "";
+					$("#answersection").html("<p>Correct!</p>");
+					hidebackgroundEU();
+	
+					} else {
+					stop();
+					wrong++;
+					userGuess = "";
+					$("#answersection").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+					hidebackgroundEU();
+				}
+			})
+				userChoice.addClass("answerchoice");
+				userChoice.attr("data-guessvalue", i)
+				userChoice.addClass("btn btn-dark btn-sm")
+				$("#answersection").append(userChoice);
+			};
+		}
 
-	// function hidebackground () {
-	// 	$("body").css("background-image", "url(" + pick.photo + ")");
-	// 	newArray.push(pick);
-	// 	questEU.splice(index,1);
+	function hidebackgroundEU () {
+		$("body").css("background-image", "url(" + pick.photo + ")");
+		newArray.push(pick);
+		questEU.splice(index,1);
 
-	// 	var hideback = setTimeout(function() {
-	// 		$("#answersection").empty();
-	// 		timer= 20;
+		var hideback = setTimeout(function() {
+			$("#answersection").empty();
+			timer= 20;
 
-	// 	if ((wrong + correct + unanswered) === questCounterEU) {
-	// 		$("#questionsection").empty();
-	// 		$("#questionsection").html("<h3>Game Over!  Here's how you did: </h3>");
-	// 		$("#answersection").append("<h4> Correct: " + correct + "</h4>" );
-	// 		$("#answersection").append("<h4> Incorrect: " + wrong + "</h4>" );
-	// 		$("#answersection").append("<h4> Unanswered: " + unanswered + "</h4>" );
-	// 		$("#reset").show();
-	// 		correct = 0;
-	// 		wrong = 0;
-	// 		unanswered = 0;
+		if ((wrong + correct + unanswered) === questCounterEU) {
+			$("#questionsection").empty();
+			$("#questionsection").html("<h3>Game Over!  Here's how you did: </h3>");
+			$("#answersection").append("<h4> Correct: " + correct + "</h4>" );
+			$("#answersection").append("<h4> Incorrect: " + wrong + "</h4>" );
+			$("#answersection").append("<h4> Unanswered: " + unanswered + "</h4>" );
+			$("#reset").show();
+			correct = 0;
+			wrong = 0;
+			unanswered = 0;
 
-	// 	} else {
-	// 		timeStart();
-	// 		displayQuestion();
+		} else {
+			timeStart();
+			displayQuestionEU();
 
-	// 	}
-	// 	}, 3000);
-	// }
+		}
+		}, 3000);
+	}
